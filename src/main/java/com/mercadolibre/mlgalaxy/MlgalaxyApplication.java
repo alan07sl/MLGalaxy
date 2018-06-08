7ea7276048451adf8483decfc46bb670ecb03047
Alan Hryniewicz <alan07sl@gmail.com>
2018-06-08 15:37:26 -0300

    Initial commit

---
+++ src/main/java/com/mercadolibre/mlgalaxy/MlgalaxyApplication.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.mlgalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlgalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlgalaxyApplication.class, args);
+	}
+}
